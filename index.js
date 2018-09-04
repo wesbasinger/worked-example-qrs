898d7e5ca029aeac30e95a6bb2027662042b3107
Wes Basinger <wbasinger@villagetechschools.org>
2018-09-03 20:30:07 -0500

    Created QRS for segment addition.

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const qr = require("qr-image");
+
+const GITHUB_STEM = "https://raw.githubusercontent.com/wesbasinger/worked-example-qrs/master/";
+
+const IMG_DIR = "segment-addition";
+
+const FULL_DIR = path.join(__dirname, IMG_DIR)
+
+const imgs = fs.readdirSync(FULL_DIR)
+
+const fullLinks = []
+
+let idx = 1;
+
+imgs.forEach((img) => {
+	
+	const composedLink = `${GITHUB_STEM}${IMG_DIR}/${img}`;
+
+	const qr_png = qr.image(composedLink, {type: "png"});
+
+	qr_png.pipe(require("fs").createWriteStream(`${FULL_DIR}/__${idx}__.png`));
+
+	idx ++
+
+})
