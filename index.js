2735906ec62270871c1ca6fa8af83fd2f7d6ff3a
Wes Basinger <wbasinger@villagetechschools.org>
2018-09-10 22:49:49 -0500

    Angle relationships.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const qr = require("qr-image");
 
 const GITHUB_STEM = "https://raw.githubusercontent.com/wesbasinger/worked-example-qrs/master/";
 
-const IMG_DIR = "segment-addition";
+const IMG_DIR = "angle-relationships";
 
 const FULL_DIR = path.join(__dirname, IMG_DIR)
 
